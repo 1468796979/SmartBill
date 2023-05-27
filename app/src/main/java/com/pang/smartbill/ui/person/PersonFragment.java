d1ca8963c2a91be150627e7a7dc02ce49aa61cbc
1468796979 <97212843+1468796979@users.noreply.github.com>
2023-05-28 06:36:53 +0800

    1

---
+++ app/src/main/java/com/pang/smartbill/ui/person/PersonFragment.java
@@ -0,0 +1,37 @@
+package com.pang.smartbill.ui.person;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pang.smartbill.databinding.FragmentPersonBinding;
+
+public class PersonFragment extends Fragment {
+
+    private FragmentPersonBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        PersonViewModel personViewModel =
+                new ViewModelProvider(this).get(PersonViewModel.class);
+
+        binding = FragmentPersonBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textPerson;
+        personViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
